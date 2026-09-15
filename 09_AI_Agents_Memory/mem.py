62725dac6a7095e91067f8d870f0b5206289f810
The-Tourist-170 <ayushya.official@gmail.com>
2026-09-15 16:53:54 +0530

    Memory aware ai agents. Sending context to LLM model.

--- 09_AI_Agents_Memory/mem.py
+++ 09_AI_Agents_Memory/mem.py
@@ -1,5 +1,6 @@
 from mem0 import Memory
 from openai import OpenAI
+import json
 
 config = {
     "version": "v1.1",
@@ -22,6 +23,8 @@ config = {
         "config": {
             "host": "localhost",
             "port": 6333,
+            "collection_name": "mem0_minilm",
+            "embedding_model_dims": 384, 
         },
     },
 }
@@ -32,21 +35,39 @@ client = OpenAI(
     base_url="http://127.0.0.1:1337/v1", 
     api_key="not-needed"
 )
+while True:
+    
+    user_query = input(">>> ")
 
-user_query = input(">>>")
+    search_mem = mem_client.search(query=user_query, filters={"user_id": "ken"})
 
-res = client.chat.completions.create(
-    model="gemma-4-12B-it-MLX-6bit",
-    messages=[{"role": "user", "content": user_query}]
-)
-
-ai_res = res.choices[0].message.content
-print(ai_res)   
-
-mem_client.add(
-    messages=[
-        {"role": "user", "content": user_query},
-        {"role": "assistant", "content": ai_res},
+    memories = [
+        f"ID: {mem.get('id')}\nMemory: {mem.get('memory')}" for mem in search_mem.get('results')
     ]
-)
-print(">>> Memory Synced")
+
+    print("Mem found: >>> ", json.dumps(search_mem))
+    
+    SYSTEM_PROMPT = f"""
+        Here is the context about the user:
+            {json.dumps(search_mem)}
+    """
+    
+    res = client.chat.completions.create(
+        model="gemma-4-12B-it-MLX-6bit",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": user_query}
+        ]
+    )
+    
+    ai_res = res.choices[0].message.content
+    print(ai_res)   
+    
+    mem_client.add(
+        messages=[
+            {"role": "user", "content": user_query},
+            {"role": "assistant", "content": ai_res},
+        ],
+        user_id="ken"
+    )
+    print(">>> Memory Synced\n")
